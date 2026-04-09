ff1dfb85b9719c79b38704c09c59f35a43d892ad
Wale <wahlezy+devops@gmail.com>
2026-04-09 05:14:53 +0100

    updtaed root variable file

--- variable.tf
+++ variable.tf
@@ -7,18 +7,3 @@ variable "key_name" {
   type        = string
 }
 
-variable "vpc_id" {
-  description = "VPC ID where resources will be created"
-  type        = string
-}
-
-variable "subnet_ids" {
-  description = "List of subnet IDs (must be in at least 2 different AZs for EKS)"
-  type        = list(string)
-}
-
-variable "vpc_cidr" {
-  description = "CIDR block for VPC"
-  type        = string
-  default     = "10.0.0.0/16"
-}
